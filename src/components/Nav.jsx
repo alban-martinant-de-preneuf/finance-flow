678aa38207088cc009db6faa8becfdfd9705390a
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-11-06 14:00:17 +0100

    principales components done

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <NavBtn type="home" />
+                <NavBtn type="transactions" />
+                <NavBtn type="recurrent" />
+                <NavBtn type="settings" />
+            </ul>
+        </nav>
+    )
+}
