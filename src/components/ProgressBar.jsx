3e572e7b573a9eaf8611fd11dfb968ec9a14ef05
alban-martinant-de-preneuf <alban.martinant-de-preneuf@laplateforme.io>
2023-11-13 16:27:20 +0100

    progressbar done

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,28 @@
+import { PieChart, Pie, Cell } from 'recharts';
+
 export default function ProgressBar() {
-    return (
-        <div className="progress-bar">
-            <div className="progress-bar__fill"></div>
-        </div>
-    )
+
+
+
+    const data = [
+        { name: 'add', value: 80, color: 'green' },
+        { name: 'min', value: 20, color: 'red' },
+      ];
+
+      return (
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx={200}
+            cy={200}
+            innerRadius={60}
+            outerRadius={80}
+            blendStroke
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.color} />
+            ))}
+          </Pie>
+        </PieChart>
+      );
 }
